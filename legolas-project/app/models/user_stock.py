780b2de6567c14dd24deb472f8ab3d08d8272ae0
Ali Keshanian <ar.k@outlook.com>
2023-11-18 08:40:25 -0800

    created UseStock model

---
+++ legolas-project/app/models/user_stock.py
@@ -0,0 +1,17 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+class UserStock(db.Model):
+    __tablename__ = 'user_stocks'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    company_id = db.Column(db.Integer, db.ForeignKey(
+        'companies.id'), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    shares = db.Column(db.Float, nullable=False)
