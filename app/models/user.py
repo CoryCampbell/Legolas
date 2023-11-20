f82a90d3e1985c1da2656484d63efffe0d618428
Quinlan <quinnbush5@gmail.com>
2023-11-20 14:24:41 -0800

    minor user fixes

--- app/models/user.py
+++ app/models/user.py
@@ -4,10 +4,10 @@ from flask_login import UserMixin
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
+        __table_args__ = {"schema": SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
@@ -15,9 +15,9 @@ class User(db.Model, UserMixin):
     hashed_password = db.Column(db.String(255), nullable=False)
 
     # Relationships
-    transactions = db.relationship('Transaction', backref='user')
-    stocks = db.relationship('UserStock', backref='user')
-    watchlist = db.relationship('Watchlist', backref='user')
+    transactions = db.relationship("Transaction", backref="user")
+    stocks = db.relationship("UserStock", backref="user")
+    watchlist = db.relationship("Watchlist", backref="user")
 
     @property
     def password(self):
@@ -31,8 +31,4 @@ class User(db.Model, UserMixin):
         return check_password_hash(self.password, password)
 
     def to_dict(self):
-        return {
-            'id': self.id,
-            'username': self.username,
-            'email': self.email
-        }
+        return {"id": self.id, "username": self.username, "email": self.email}
