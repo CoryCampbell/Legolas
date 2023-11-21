c18d960d9b3c8fd7c8f4306fb2de21022e8e0ecf
CoryCampbell <corycampbell20@yahoo.com>
2023-11-21 15:18:36 -0600

    portfolio styling started

--- react-app/src/components/Portfolio/index.js
+++ react-app/src/components/Portfolio/index.js
@@ -7,20 +7,19 @@ const Portfolio = () => {
   const sessionUser = useSelector((state) => state.session.user);
 
   return (
-    <div className="portfolio">
-      {sessionUser ? (
-        <div>
-          <div className="user-info">
-            <h2>Hello {sessionUser.username}</h2>
-            <h3 className="portfolio-value">Portfolio Value: $4,227.45</h3>
-          </div>
-          <LineChart />
-        </div>
-      ) : (
-        <Landing />
-      )}
-    </div>
-  );
+		<div className="portfolio">
+			{sessionUser ? (
+				<div>
+					<div className="user-info">
+						<h3 className="portfolio-value">${sessionUser.balance}</h3>
+					</div>
+					<LineChart />
+				</div>
+			) : (
+				<Landing />
+			)}
+		</div>
+	);
 };
 
 export default Portfolio;
