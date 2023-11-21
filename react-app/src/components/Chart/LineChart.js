c5b86f25e75da2f4b2821f60aa1e71a4603eff1b
CoryCampbell <corycampbell20@yahoo.com>
2023-11-21 16:37:19 -0600

    portfolio frontend addtions

--- react-app/src/components/Chart/LineChart.js
+++ react-app/src/components/Chart/LineChart.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 import { StocksData } from "../../fake-data/data";
+import "./chart.css";
 
 // ChartJS.register(CategoryScale, ArcElement, Tooltip, Legend);
 
--- react-app/src/components/Portfolio/index.js
+++ react-app/src/components/Portfolio/index.js
@@ -7,14 +7,27 @@ const Portfolio = () => {
   const sessionUser = useSelector((state) => state.session.user);
 
   return (
-		<div className="portfolio">
+		<div className="main-portfolio-container">
 			{sessionUser ? (
-				<div>
-					<div className="user-info">
-						<h3 className="portfolio-value">${sessionUser.balance}</h3>
+				<>
+					<div className="main-left-container">
+						<div className="user-info">
+							<h1 className="portfolio-value">${sessionUser.balance}</h1>
+						</div>
+						<LineChart />
+						<div className="mod about-mod">ABOUT</div>
+						<div className="mod discover-mod">DISCOVER</div>
+						<div className="mod trending-mod">TRENDING</div>
+						<div className="mod learning-mod">LEARNING</div>
+						<div className="mod news-mod">NEWS</div>
 					</div>
-					<LineChart />
-				</div>
+					<div className="main-list-container">
+						Lists
+						<div className="watchlist">Watchlist 1</div>
+						<div className="watchlist">Watchlist 2</div>
+						<div className="watchlist">Watchlist 3</div>
+					</div>
+				</>
 			) : (
 				<Landing />
 			)}
