e03765082591a6348ea01118985b359a3d1746e6
Quinlan <quinnbush5@gmail.com>
2023-11-21 09:21:47 -0800

    icon fix, cursor pointer fix

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,39 +1,43 @@
-import React from 'react';
-import { NavLink, useHistory } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import './Navigation.css';
-import logo from '../../static/test-logos-and-images/legolas-sample2.png'
+import React from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
+import logo from "../../static/test-logos-and-images/legolas-sample2.png";
 
-function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
-	const history = useHistory();
-	return (
-		<nav>
-		<div className='navlinks-container'>
-			<div className='logo-container'>
-				<NavLink exact to="/">
-					<img className='logo' src={logo} alt='logo'/>
-				</NavLink>
-			</div>
-		</div>
-		<div className='auth-container'>
-			<div className='nav-buttons-container'>
-				<div>
-					<NavLink exact to="/login"><button className='nav-login-button'>Log In</button></NavLink>
-				</div>
-				<div>
-				<NavLink exact to="/signup"><button className='nav-signup-button'>Sign Up</button></NavLink>
-				</div>
-				{/* {isLoaded && (
-					<li>
-						<ProfileButton user={sessionUser} />
-					</li>
-				)} */}
-			</div>
-		</div>
-		</nav>
-	);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
+  const history = useHistory();
+  return (
+    <nav>
+      <div className="navlinks-container">
+        <div className="logo-container">
+          <NavLink exact to="/">
+            <img className="logo" src={logo} alt="logo" />
+          </NavLink>
+        </div>
+      </div>
+      <div className="auth-container">
+        <div className="nav-buttons-container">
+          <div>
+            <NavLink exact to="/login">
+              <button className="nav-login-button">Log In</button>
+            </NavLink>
+          </div>
+          <div>
+            <NavLink exact to="/signup">
+              <button className="nav-signup-button">Sign Up</button>
+            </NavLink>
+          </div>
+          {isLoaded && (
+            <li>
+              <ProfileButton user={sessionUser} />
+            </li>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navigation;
