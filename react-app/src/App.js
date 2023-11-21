aba0cfe63167ca6224d07b4889d4acbba95d2e4f
Malcolm Caleb <malcolmcaleb01@gmail.com>
2023-11-20 19:31:25 -0500

    landing page fix

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,6 +5,7 @@ import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
+import Landing from "./components/LandingPage";
 
 function App() {
   const dispatch = useDispatch();
@@ -24,6 +25,9 @@ function App() {
           <Route path="/signup">
             <SignupFormPage />
           </Route>
+          <Route exact path='/'>
+            <Landing />
+            </Route>
         </Switch>
       )}
     </>
