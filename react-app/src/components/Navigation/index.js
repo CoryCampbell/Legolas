626e3ccf1775085095fb591aa346ebfc4c8e84d8
Ali Keshanian <ar.k@outlook.com>
2023-11-22 12:51:30 -0800

    add search input field to the nav bar, add css for styling

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -7,43 +7,46 @@ import logo from "../../static/test-logos-and-images/Legolas-logo-dark.png";
 import logoHover from "../../static/test-logos-and-images/Legolas-logo-final.png";
 
 function Navigation({ isLoaded }) {
-	const sessionUser = useSelector((state) => state.session.user);
-	const history = useHistory();
-	//   console.log(sessionUser);
-	return (
-		<nav>
-			<div className="navlinks-container">
-				<div className="logo-container">
-					<NavLink exact to="/">
-						<img className="logo" src={logo} alt="logo" />
-						<img className="logo-hover" src={logoHover} alt="logo" />
-					</NavLink>
-				</div>
-			</div>
-			{!sessionUser ? (
-				<div className="auth-container">
-					<div className="nav-buttons-container">
-						<div>
-							<NavLink exact to="/login">
-								<button className="nav-login-button">Log In</button>
-							</NavLink>
-						</div>
-						<div>
-							<NavLink exact to="/signup">
-								<button className="nav-signup-button">Sign Up</button>
-							</NavLink>
-						</div>
-					</div>
-				</div>
-			) : (
-				isLoaded && (
-					<li className="profile-button">
-						<ProfileButton user={sessionUser} isLoaded={isLoaded} />
-					</li>
-				)
-			)}
-		</nav>
-	);
+  const sessionUser = useSelector((state) => state.session.user);
+  const history = useHistory();
+  //   console.log(sessionUser);
+  return (
+    <nav>
+      <div className="navlinks-container">
+        <div className="logo-container">
+          <NavLink exact to="/">
+            <img className="logo" src={logo} alt="logo" />
+            <img className="logo-hover" src={logoHover} alt="logo" />
+          </NavLink>
+        </div>
+      </div>
+      <div className="search-bar">
+        <input type="text" />
+      </div>
+      {!sessionUser ? (
+        <div className="auth-container">
+          <div className="nav-buttons-container">
+            <div>
+              <NavLink exact to="/login">
+                <button className="nav-login-button">Log In</button>
+              </NavLink>
+            </div>
+            <div>
+              <NavLink exact to="/signup">
+                <button className="nav-signup-button">Sign Up</button>
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      ) : (
+        isLoaded && (
+          <li className="profile-button">
+            <ProfileButton user={sessionUser} isLoaded={isLoaded} />
+          </li>
+        )
+      )}
+    </nav>
+  );
 }
 
 export default Navigation;
