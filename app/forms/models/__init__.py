758b7a07c26e45d657b8adf81eb1beacf7dd7c1b
CoryCampbell <corycampbell20@yahoo.com>
2023-11-25 01:13:35 -0600

    ready for saturday

--- app/forms/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .db import db
-from .user import User
-from .transaction import Transaction
-from .company import Company
-from .portfolio import Portfolio
-from .watchlist import Watchlist
-from .watchlist_detail import Watchlist_detail
-from .db import environment, SCHEMA
