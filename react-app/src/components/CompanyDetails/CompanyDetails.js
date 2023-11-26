fca2a256f884f05cf3d2f763ac54ec289d9a1bdd
Quinlan <quinnbush5@gmail.com>
2023-11-25 19:38:01 -0800

    company details buy/sell started

--- react-app/src/components/CompanyDetails/CompanyDetails.js
+++ react-app/src/components/CompanyDetails/CompanyDetails.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCompany } from "../../store/companies";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
@@ -7,10 +7,27 @@ import "./CompanyDetails.css";
 
 export default function CompanyDetails() {
   const { company_id } = useParams();
+  const [selectedOption, setSelectedOption] = useState("dollars");
+  const [dollarAmount, setDollarAmount] = useState("");
+  const [sharesAmount, setSharesAmount] = useState("");
   const dispatch = useDispatch();
 
+  const sessionUser = useSelector((state) => state.session.user);
+  //   console.log("sessionUser", sessionUser);
   const company = useSelector((state) => state.companies.company);
-  console.log(company);
+  //   console.log(company);
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const handleDollarChange = (event) => {
+    setDollarAmount(event.target.value);
+  };
+
+  const handleShareChange = (event) => {
+    setSharesAmount(event.target.value);
+  };
 
   useEffect(() => {
     dispatch(fetchCompany(company_id));
@@ -32,10 +49,82 @@ export default function CompanyDetails() {
         </div>
 
         <div className="buy-or-sell-view">
-          <div>Buy Shares</div>
-          <div>Sell Shares</div>
+          <div className="buy-box-top">
+            <div>Buy {company.symbol}</div>
+          </div>
+          <div className="order-type-box">
+            <div>Order Type: Market Order</div>
+          </div>
+
+          <div className="buy-in-box">
+            <div>
+              Buy In:
+              <select
+                id="dropdown"
+                value={selectedOption}
+                onChange={handleOptionChange}
+              >
+                <option value="dollars">Dollars</option>
+                <option value="shares">Shares</option>
+              </select>
+              <div className="buy-in-amount-box">
+                {selectedOption === "dollars" ? (
+                  <div className="dollars-box">
+                    <div>
+                      <div className="amount-text">Amount:</div>
+                      <input
+                        type="number"
+                        placeholder="$0.00"
+                        className="input-dollars"
+                        value={dollarAmount}
+                        onChange={handleDollarChange}
+                      ></input>
+                    </div>
+                    <div className="est-box">
+                      <div className="est-quantity">Est. Quantity:</div>
+                      <div className="est-quantity-num">
+                        {(dollarAmount / company.price).toFixed(6)}
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="shares-box">
+                    <div>
+                      <div className="amount-text">Shares:</div>
+                      <input
+                        type="number"
+                        placeholder="0"
+                        className="input-shares"
+                        value={sharesAmount}
+                        onChange={handleShareChange}
+                      ></input>
+                    </div>
+                    <div className="market-price-box">
+                      <div className="market-price-box-text">Market Price:</div>
+                      <div>${company.price}</div>
+                    </div>
+                    <div className="est-box">
+                      <div className="est-quantity">Est. Cost:</div>
+                      <div className="est-quantity-num">
+                        ${(company.price * sharesAmount).toFixed(2)}
+                      </div>
+                    </div>
+                  </div>
+                )}
+                <div className="review-order-box">
+                  <div className="rev-button">
+                    <button>Review Order</button>
+                  </div>
+                </div>
+                <div className="buy-power-box">
+                  <div className="buy-power">
+                    {`$${sessionUser.balance} buying power available`}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div></div>
       </div>
     </>
   );
