5dc29cd98936f01ff1fa3ccda46557a233f04997
Quinlan <quinnbush5@gmail.com>
2023-11-27 15:05:48 -0800

    sell function almost fixed

--- react-app/src/components/CompanyDetails/CompanyDetails.js
+++ react-app/src/components/CompanyDetails/CompanyDetails.js
@@ -14,6 +14,7 @@ export default function CompanyDetails() {
   const [dollarAmount, setDollarAmount] = useState("");
   const [sharesAmount, setSharesAmount] = useState("");
   const [showButtons, setShowButtons] = useState(false);
+  const [action, setAction] = useState("buy");
   const dispatch = useDispatch();
   const history = useHistory();
 
@@ -21,6 +22,12 @@ export default function CompanyDetails() {
   //   console.log("sessionUser", sessionUser);
   const company = useSelector((state) => state.companies.company);
   //   console.log(company);
+  const portfolio = useSelector((state) =>
+    Object.values(state.portfolio.currentUserPortfolio)?.filter(
+      (stock) => stock.company_id == company_id
+    )
+  );
+  console.log("portfolio======>", portfolio[0]);
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
@@ -43,10 +50,10 @@ export default function CompanyDetails() {
     setShowButtons(true);
   };
 
-  const handleConfirmOrder = async () => {
+  const handleConfirmOrderBuy = async () => {
     // const numberOfShares = selectedOption === "shares" ? sharesAmount || 0 : 0;
 
-    console.log(sharesAmount);
+    // console.log(sharesAmount);
     try {
       const res = await fetch(`/api/purchases/${company_id}`, {
         method: "POST",
@@ -71,8 +78,38 @@ export default function CompanyDetails() {
     }
   };
 
+  const handleConfirmOrderSell = async () => {
+    console.log(sharesAmount);
+
+    try {
+      const res = await fetch(`/api/sell/${company_id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          number_of_shares_to_sell: sharesAmount,
+        }),
+      });
+      console.log("res------>", res);
+      const data = await res.json();
+
+      console.log("data ------>", data);
+
+      setDollarAmount("");
+      setSharesAmount("");
+      setShowButtons(false);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   const handleCancelOrder = () => {
     setShowButtons(false);
+  };
+
+  const handleActionChange = (e) => {
+    setAction(e.target.value);
   };
 
   useEffect(() => {
@@ -96,97 +133,202 @@ export default function CompanyDetails() {
 
         <div className="buy-or-sell-view">
           <div className="buy-box-top">
-            <div>Buy {company.symbol}</div>
+            <div>
+              <select value={action} onChange={handleActionChange}>
+                <option value="buy">Buy</option>
+                <option value="sell">Sell</option>
+              </select>
+              {` ${company.symbol}`}
+            </div>
           </div>
           <div className="order-type-box">
             <div>Order Type: Market Order</div>
           </div>
-
-          <div className="buy-in-box">
-            <div>
-              Buy In:
-              <select
-                id="dropdown"
-                value={selectedOption}
-                onChange={handleOptionChange}
-              >
-                <option value="dollars">Dollars</option>
-                <option value="shares">Shares</option>
-              </select>
-              <div className="buy-in-amount-box">
-                {selectedOption === "dollars" ? (
-                  <div className="dollars-box">
-                    <div>
-                      <div className="amount-text">Amount:</div>
-                      <input
-                        type="number"
-                        placeholder="$0.00"
-                        className="input-dollars"
-                        value={dollarAmount}
-                        onChange={handleDollarChange}
-                      ></input>
-                    </div>
-                    <div className="est-box">
-                      <div className="est-quantity">Est. Quantity:</div>
-                      <div className="est-quantity-num">
-                        {dollarAmount > 0
-                          ? (dollarAmount / company.price).toFixed(6)
-                          : 0}
+          {action === "buy" ? (
+            <div className="buy-in-box">
+              <div>
+                Buy In:
+                <select
+                  id="dropdown"
+                  value={selectedOption}
+                  onChange={handleOptionChange}
+                >
+                  <option value="dollars">Dollars</option>
+                  <option value="shares">Shares</option>
+                </select>
+                <div className="buy-in-amount-box">
+                  {selectedOption === "dollars" ? (
+                    <div className="dollars-box">
+                      <div>
+                        <div className="amount-text">Amount:</div>
+                        <input
+                          type="number"
+                          placeholder="$0.00"
+                          className="input-dollars"
+                          value={dollarAmount}
+                          onChange={handleDollarChange}
+                        ></input>
+                      </div>
+                      <div className="est-box">
+                        <div className="est-quantity">Est. Quantity:</div>
+                        <div className="est-quantity-num">
+                          {dollarAmount > 0
+                            ? (dollarAmount / company.price).toFixed(6)
+                            : 0}
+                        </div>
                       </div>
                     </div>
-                  </div>
-                ) : (
-                  <div className="shares-box">
-                    <div>
-                      <div className="amount-text">Shares:</div>
-                      <input
-                        type="number"
-                        placeholder="0"
-                        className="input-shares"
-                        value={sharesAmount}
-                        onChange={handleShareChange}
-                      ></input>
-                    </div>
-                    <div className="market-price-box">
-                      <div className="market-price-box-text">Market Price:</div>
-                      <div>${company.price}</div>
-                    </div>
-                    <div className="est-box">
-                      <div className="est-quantity">Est. Cost:</div>
-                      <div className="est-quantity-num">
-                        $
-                        {sharesAmount > 0
-                          ? (company.price * sharesAmount).toFixed(2)
-                          : 0}
+                  ) : (
+                    <div className="shares-box">
+                      <div>
+                        <div className="amount-text">Shares:</div>
+                        <input
+                          type="number"
+                          placeholder="0"
+                          className="input-shares"
+                          value={sharesAmount}
+                          onChange={handleShareChange}
+                        ></input>
                       </div>
-                    </div>
-                  </div>
-                )}
-                <div className="review-order-box">
-                  <div className="rev-button">
-                    {!showButtons && (
-                      <button onClick={handleReviewOrder}>Review Order</button>
-                    )}
-                  </div>
-                  {showButtons && (
-                    <div className="confirm-cancel-buttons">
-                      <button onClick={handleConfirmOrder}>
-                        Confirm Order
-                      </button>
-                      <button onClick={handleCancelOrder}>Cancel Order</button>
+                      <div className="market-price-box">
+                        <div className="market-price-box-text">
+                          Market Price:
+                        </div>
+                        <div>${company.price}</div>
+                      </div>
+                      <div className="est-box">
+                        <div className="est-quantity">Est. Cost:</div>
+                        <div className="est-quantity-num">
+                          $
+                          {sharesAmount > 0
+                            ? (company.price * sharesAmount).toFixed(2)
+                            : 0}
+                        </div>
+                      </div>
                     </div>
                   )}
-                </div>
-                <div className="buy-power-box">
-                  <div className="buy-power">
-                    {`$${sessionUser.balance.toFixed(
-                      2
-                    )} buying power available`}
+                  <div className="review-order-box">
+                    <div className="rev-button">
+                      {!showButtons && (
+                        <button onClick={handleReviewOrder}>
+                          Review Order
+                        </button>
+                      )}
+                    </div>
+                    {showButtons && (
+                      <div className="confirm-cancel-buttons">
+                        <button onClick={handleConfirmOrderBuy}>
+                          Confirm Order
+                        </button>
+                        <button onClick={handleCancelOrder}>
+                          Cancel Order
+                        </button>
+                      </div>
+                    )}
+                  </div>
+                  <div className="buy-power-box">
+                    <div className="buy-power">
+                      {`$${sessionUser.balance.toFixed(
+                        2
+                      )} buying power available`}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          ) : (
+            <div className="buy-in-box">
+              <div>
+                Sell In:
+                <select
+                  id="dropdown"
+                  value={selectedOption}
+                  onChange={handleOptionChange}
+                >
+                  <option value="dollars">Dollars</option>
+                  <option value="shares">Shares</option>
+                </select>
+                <div className="buy-in-amount-box">
+                  {selectedOption === "dollars" ? (
+                    <div className="dollars-box">
+                      <div>
+                        <div className="amount-text">Amount:</div>
+                        <input
+                          type="number"
+                          placeholder="$0.00"
+                          className="input-dollars"
+                          value={dollarAmount}
+                          onChange={handleDollarChange}
+                        ></input>
+                      </div>
+                      <div className="est-box">
+                        <div className="est-quantity">Est. Quantity:</div>
+                        <div className="est-quantity-num">
+                          {dollarAmount > 0
+                            ? (dollarAmount / company.price).toFixed(6)
+                            : 0}
+                        </div>
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="shares-box">
+                      <div>
+                        <div className="amount-text">Shares:</div>
+                        <input
+                          type="number"
+                          placeholder="0"
+                          className="input-shares"
+                          value={sharesAmount}
+                          onChange={handleShareChange}
+                        ></input>
+                      </div>
+                      <div className="market-price-box">
+                        <div className="market-price-box-text">
+                          Market Price:
+                        </div>
+                        <div>${company.price}</div>
+                      </div>
+                      <div className="est-box">
+                        <div className="est-quantity">Est. Credit:</div>
+                        <div className="est-quantity-num">
+                          $
+                          {sharesAmount > 0
+                            ? (company.price * sharesAmount).toFixed(2)
+                            : 0}
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                  <div className="review-order-box">
+                    <div className="rev-button">
+                      {!showButtons && (
+                        <button onClick={handleReviewOrder}>
+                          Review Order
+                        </button>
+                      )}
+                    </div>
+                    {showButtons && (
+                      <div className="confirm-cancel-buttons">
+                        <button onClick={handleConfirmOrderSell}>
+                          Confirm Order
+                        </button>
+                        <button onClick={handleCancelOrder}>
+                          Cancel Order
+                        </button>
+                      </div>
+                    )}
+                  </div>
+                  <div className="buy-power-box">
+                    <div className="buy-power">
+                      {selectedOption === "dollars"
+                        ? `$${portfolio[0]?.price.toFixed(2)} Available`
+                        : `${portfolio[0]?.shares.toFixed(6)} Shares Available`}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
