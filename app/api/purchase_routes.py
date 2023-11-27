f439fc2bf56ace5276b11e5d4ded9f7a9fea5aac
Quinlan <quinnbush5@gmail.com>
2023-11-27 11:31:27 -0800

    buy fix

--- app/api/purchase_routes.py
+++ app/api/purchase_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from datetime import datetime
-from app.models import db, User, Company, Transaction, Portfolio
+from app.models import db, User, Company, Transaction, Portfolio, Stocks_owned
 from flask_login import login_required, current_user
 
 purchase_routes = Blueprint("purchases", __name__)
@@ -15,9 +15,11 @@ def purchase(company_id):
 
     # Validate the data
     if float(number_of_shares) <= 0:
+        print("py shares ------>", float(number_of_shares))
         return jsonify({"error": "Invalid number of shares"}), 400
 
     company = Company.query.get(company_id)
+    print("company-------->", company)
 
     if not company:
         return jsonify({"error": "Company does not exist"}), 404
@@ -25,7 +27,9 @@ def purchase(company_id):
     if not user:
         return jsonify({"error": "User does not exist"}), 400
 
-    if user.balance < company.price * float(number_of_shares):
+    price_per_share = round(company.price * float(number_of_shares), 2)
+
+    if user.balance < price_per_share:
         return (
             jsonify({"error": "Insufficient funds, transaction cannot be processed"}),
             400,
@@ -37,7 +41,7 @@ def purchase(company_id):
     ).first()
     if portfolio:
         portfolio.shares += float(number_of_shares)
-        # portfolio.price += company.price * float(number_of_shares)
+        portfolio.price += price_per_share
         print(portfolio.shares, "==============> USER SHARES")
         # print(new_stock.price, "==============> USER PRICE")
     else:
@@ -45,27 +49,46 @@ def purchase(company_id):
             user_id=user_id,
             company_id=company_id,
             shares=float(number_of_shares),
-            price=company.price * float(number_of_shares),
+            price=price_per_share,
         )
         print(new_stock.price, "==============> NEW STOCK")
         db.session.add(new_stock)
+
+    # Update or create StocksOwned
+    stocks_owned = Stocks_owned.query.filter_by(
+        user_id=user_id, company_id=company_id
+    ).first()
+    if stocks_owned:
+        stocks_owned.amt_shares = stocks_owned.amt_shares + round(
+            float(number_of_shares), 6
+        )
+        stocks_owned.stock_price = company.price
+
+    else:
+        new_stocks_owned = Stocks_owned(
+            user_id=user_id,
+            company_id=company_id,
+            amt_shares=round(float(number_of_shares), 6),
+            stock_price=company.price,
+        )
+        db.session.add(new_stocks_owned)
 
     # Create a transaction
     new_transaction = Transaction(
         user_id=user_id,
         company_id=company_id,
-        total=company.price * float(number_of_shares),
+        total=price_per_share,
         type="buy",
         created_at=datetime.now(),
         updated_at=datetime.now(),
     )
     db.session.add(new_transaction)
-    db.session.add(new_stock)
+    # db.session.add(new_stock)
 
     print(new_transaction, "===========> NEW TRANSACTION")
 
     # Update user's balance
-    user.balance -= company.price * float(number_of_shares)
+    user.balance -= price_per_share
 
     print(user.balance, "==============> USER BALANCE")
 
--- react-app/src/components/Portfolio/index.js
+++ react-app/src/components/Portfolio/index.js
@@ -9,81 +9,93 @@ import Watchlist from "../Watchlists";
 import { fetchAllWatchlists } from "../../store/watchlists";
 
 const Portfolio = () => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const sessionUser = useSelector((state) => state.session.user);
-	console.log("sessionUser", sessionUser);
+  const sessionUser = useSelector((state) => state.session.user);
+  // console.log("sessionUser", sessionUser);
 
-	const userPortfolio = useSelector((state) => Object.values(state.portfolio?.currentUserPortfolio));
-	console.log("userPortfolio", userPortfolio);
+  const userPortfolio = useSelector((state) =>
+    Object.values(state.portfolio?.currentUserPortfolio)
+  );
+  console.log("userPortfolio", userPortfolio);
 
-	let totalPortfolioValue = 0;
+  let totalPortfolioValue = 0;
 
-	userPortfolio?.map((stock) => {
-		totalPortfolioValue += stock.price * stock.shares;
-		console.log("totalPortfolioValue", totalPortfolioValue);
-	});
+  userPortfolio?.map((stock) => {
+    totalPortfolioValue += stock.price;
+    console.log("totalPortfolioValue", totalPortfolioValue);
+  });
 
-	useEffect(() => {
-		dispatch(fetchUserPortfolio(sessionUser?.id));
-	}, [dispatch, sessionUser?.id]);
+  useEffect(() => {
+    dispatch(fetchUserPortfolio(sessionUser?.id));
+  }, [dispatch, sessionUser?.id]);
 
-	return (
-		<div className="main-portfolio-container">
-			{sessionUser && userPortfolio ? (
-				<>
-					<div className="main-left-container">
-						<div className="user-info">
-							<h2 className="portfolio-value">${totalPortfolioValue.toFixed(2)}</h2>
-						</div>
-						<LineChart />
+  return (
+    <div className="main-portfolio-container">
+      {sessionUser && userPortfolio ? (
+        <>
+          <div className="main-left-container">
+            <div className="user-info">
+              <h2 className="portfolio-value">
+                ${totalPortfolioValue.toFixed(2)}
+              </h2>
+            </div>
+            <LineChart />
 
-						<div>Buying Power: ${sessionUser.balance.toFixed(2)}</div>
+            <div>Buying Power: ${sessionUser.balance.toFixed(2)}</div>
 
-						<div className="mod discover-mod">
-							DISCOVER
-							<div>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-								dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-								ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-								fugiat nulla
-							</div>
-						</div>
-						<div className="mod trending-mod">
-							TRENDING
-							<div>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-								dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-								ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-								fugiat nulla
-							</div>
-						</div>
-						<div className="mod learning-mod">
-							LEARNING
-							<div>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-								dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-								ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-								fugiat nulla
-							</div>
-						</div>
-						<div className="mod news-mod">
-							NEWS
-							<div>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-								dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-								ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-								fugiat nulla
-							</div>
-						</div>
-					</div>
-					<Watchlist />
-				</>
-			) : (
-				<Landing />
-			)}
-		</div>
-	);
+            <div className="mod discover-mod">
+              DISCOVER
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla
+              </div>
+            </div>
+            <div className="mod trending-mod">
+              TRENDING
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla
+              </div>
+            </div>
+            <div className="mod learning-mod">
+              LEARNING
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla
+              </div>
+            </div>
+            <div className="mod news-mod">
+              NEWS
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla
+              </div>
+            </div>
+          </div>
+          <Watchlist />
+        </>
+      ) : (
+        <Landing />
+      )}
+    </div>
+  );
 };
 
 export default Portfolio;
