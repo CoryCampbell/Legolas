53d011f1d6b03e32258e8562e49dab195f542acc
CoryCampbell <corycampbell20@yahoo.com>
2023-11-27 13:13:41 -0600

    updates to create new watchlist

--- react-app/src/components/WatchlistModal/index.js
+++ react-app/src/components/WatchlistModal/index.js
@@ -1,38 +1,48 @@
 import { useState } from "react"
 import { useModal } from "../../context/Modal"
+import { useDispatch } from "react-redux";
+import { addNewWatchlistThunk } from "../../store/watchlists";
 
 function WatchListModal() {
-    const [watchlistName, setWatchlistName] = useState("");
-    const [companySymbols, setCompanySymbols] = useState([])
-    const { closeModal } = useModal()
+	const dispatch = useDispatch();
+	const [watchlistName, setWatchlistName] = useState("");
+	const [errors, setErrors] = useState([]);
+	// const [companySymbols, setCompanySymbols] = useState([]);
+	const { closeModal } = useModal();
 
-    const handleSubmit = async (e) => {
-			e.preventDefault();
-			closeModal();
-		};
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const data = await dispatch(addNewWatchlistThunk(watchlistName));
+		if (data) {
+			setErrors(data);
+		}
+		closeModal();
 
-    return (
-			<>
-				<h1>Create a Watchlist</h1>
-				<form>
-					<input
-						placeholder="Watchlist name"
-						type="text"
-						value={watchlistName}
-						onChange={(e) => setWatchlistName(e.target.value)}
-						required
-					/>
-					{/* <input
+		setWatchlistName("");
+	};
+
+	return (
+		<>
+			<h1>Create a Watchlist</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					placeholder="Watchlist name"
+					type="text"
+					value={watchlistName}
+					onChange={(e) => setWatchlistName(e.target.value)}
+					required
+				/>
+				{/* <input
                 placeholder="Company name's (seperated by commas)"
                 type="text"
                 value={companySymbols}
                 onChange={(e) => setCompanySymbols(e.target.value)}
                 required
             /> */}
-					<button type="submit">Create Watchlist</button>
-				</form>
-			</>
-		);
+				<button type="submit">Create Watchlist</button>
+			</form>
+		</>
+	);
 }
 
 export default WatchListModal
