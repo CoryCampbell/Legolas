3777319960b398160a10fae7403e4ec0b7e769ac
CoryCampbell <corycampbell20@yahoo.com>
2023-11-27 21:59:25 -0600

    lil bit of sauce

--- react-app/src/components/CompanyDetails/CompanyDetails.js
+++ react-app/src/components/CompanyDetails/CompanyDetails.js
@@ -134,233 +134,188 @@ export default function CompanyDetails() {
   }, [dispatch, company_id, sessionUser?.id]);
 
   return (
-    <>
-      <div className="full-window-view">
-        <div className="stock-view">
-          <div className="company-detail-name">{company.name}</div>
+		<>
+			<div className="full-window-view">
+				<div className="stock-view">
+					<div className="company-detail-name">{company.name}</div>
 
-          <div className="company-detail-price">${company.price}</div>
+					<div className="company-detail-price">${company.price}</div>
 
-          <div className="company-detail-chart">
-            <LineChart />
-          </div>
+					<div className="company-detail-chart">
+						<LineChart />
+					</div>
 
-          <div className="company-detail-about">{company.about}</div>
-        </div>
+					<div className="company-detail-about">{company.about}</div>
+				</div>
 
-        <div className="buy-or-sell-view">
-          <div className="buy-box-top">
-            <div>
-              <select value={action} onChange={handleActionChange}>
-                <option value="buy">Buy</option>
-                <option value="sell">Sell</option>
-              </select>
-              {` ${company.symbol}`}
-            </div>
-          </div>
-          <div className="order-type-box">
-            <div>Order Type: Market Order</div>
-          </div>
-          {action === "buy" ? (
-            <div className="buy-in-box">
-              <div>
-                Buy In:
-                <select
-                  id="dropdown"
-                  value={selectedOption}
-                  onChange={handleOptionChange}
-                >
-                  <option value="dollars">Dollars</option>
-                  <option value="shares">Shares</option>
-                </select>
-                <div className="buy-in-amount-box">
-                  {selectedOption === "dollars" ? (
-                    <div className="dollars-box">
-                      <div>
-                        <div className="amount-text">Amount:</div>
-                        <input
-                          type="number"
-                          placeholder="$0.00"
-                          className="input-dollars"
-                          value={dollarAmount}
-                          onChange={handleDollarChange}
-                        ></input>
-                      </div>
-                      <div className="est-box">
-                        <div className="est-quantity">Est. Quantity:</div>
-                        <div className="est-quantity-num">
-                          {dollarAmount > 0
-                            ? (dollarAmount / company.price).toFixed(6)
-                            : 0}
-                        </div>
-                      </div>
-                    </div>
-                  ) : (
-                    <div className="shares-box">
-                      <div>
-                        <div className="amount-text">Shares:</div>
-                        <input
-                          type="number"
-                          placeholder="0"
-                          className="input-shares"
-                          value={sharesAmount}
-                          onChange={handleShareChange}
-                        ></input>
-                      </div>
-                      <div className="market-price-box">
-                        <div className="market-price-box-text">
-                          Market Price:
-                        </div>
-                        <div>${company.price}</div>
-                      </div>
-                      <div className="est-box">
-                        <div className="est-quantity">Est. Cost:</div>
-                        <div className="est-quantity-num">
-                          $
-                          {sharesAmount > 0
-                            ? (company.price * sharesAmount).toFixed(2)
-                            : 0}
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                  <div className="review-order-box">
-                    <div className="rev-button">
-                      {!showButtons && (
-                        <button onClick={handleReviewOrder}>
-                          Review Order
-                        </button>
-                      )}
-                    </div>
-                    {showButtons && (
-                      <div className="confirm-cancel-buttons">
-                        <button onClick={handleConfirmOrderBuy}>
-                          Confirm Order
-                        </button>
-                        <button onClick={handleCancelOrder}>
-                          Cancel Order
-                        </button>
-                      </div>
-                    )}
-                  </div>
-                  <div className="buy-power-box">
-                    <div className="buy-power">
-                      {`$${sessionUser.balance.toFixed(
-                        2
-                      )} buying power available`}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ) : (
-            <div className="buy-in-box">
-              <div>
-                Sell In:
-                <select
-                  id="dropdown"
-                  value={selectedOption}
-                  onChange={handleOptionChange}
-                >
-                  <option value="dollars">Dollars</option>
-                  <option value="shares">Shares</option>
-                </select>
-                <div className="buy-in-amount-box">
-                  {selectedOption === "dollars" ? (
-                    <div className="dollars-box">
-                      <div>
-                        <div className="amount-text">Amount:</div>
-                        <input
-                          type="number"
-                          placeholder="$0.00"
-                          className="input-dollars"
-                          value={dollarAmount}
-                          onChange={handleDollarChange}
-                        ></input>
-                      </div>
-                      <div className="est-box">
-                        <div className="est-quantity">Est. Quantity:</div>
-                        <div className="est-quantity-num">
-                          {dollarAmount > 0
-                            ? (dollarAmount / company.price).toFixed(6)
-                            : 0}
-                        </div>
-                      </div>
-                    </div>
-                  ) : (
-                    <div className="shares-box">
-                      <div>
-                        <div className="amount-text">Shares:</div>
-                        <input
-                          type="number"
-                          placeholder="0"
-                          className="input-shares"
-                          value={sharesAmount}
-                          onChange={handleShareChange}
-                        ></input>
-                      </div>
-                      <div className="market-price-box">
-                        <div className="market-price-box-text">
-                          Market Price:
-                        </div>
-                        <div>${company.price}</div>
-                      </div>
-                      <div className="est-box">
-                        <div className="est-quantity">Est. Credit:</div>
-                        <div className="est-quantity-num">
-                          $
-                          {sharesAmount > 0
-                            ? (company.price * sharesAmount).toFixed(2)
-                            : 0}
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                  <div className="review-order-box">
-                    <div className="rev-button">
-                      {!showButtons && (
-                        <button onClick={handleReviewOrder}>
-                          Review Order
-                        </button>
-                      )}
-                    </div>
-                    {showButtons && (
-                      <div className="confirm-cancel-buttons">
-                        <button onClick={handleConfirmOrderSell}>
-                          Confirm Order
-                        </button>
-                        <button onClick={handleCancelOrder}>
-                          Cancel Order
-                        </button>
-                      </div>
-                    )}
-                  </div>
-                  <div className="buy-power-box">
-                    <div className="buy-power">
-                      {portfolio[0].price > 0
-                        ? selectedOption === "dollars"
-                          ? `$${portfolio[0]?.price.toFixed(2)} Available`
-                          : `${portfolio[0]?.shares.toFixed(
-                              6
-                            )} Shares Available`
-                        : selectedOption === "dollars"
-                        ? "0 Available"
-                        : "0 Shares Available"}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
-
-          <div>
-            <OpenModalButton
-              buttonText={"+ Add to Lists"}
-              modalComponent={<AddToWatchlistModal company_id={company_id} />}
-            />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+				<div className="buy-or-sell-view">
+					<div className="buy-box-top">
+						<div>
+							<select value={action} onChange={handleActionChange}>
+								<option value="buy">Buy</option>
+								<option value="sell">Sell</option>
+							</select>
+							{` ${company.symbol}`}
+						</div>
+					</div>
+					<div className="order-type-box">
+						<div>Order Type: Market Order</div>
+					</div>
+					{action === "buy" ? (
+						<div className="buy-in-box">
+							<div>
+								Buy In:
+								<select id="dropdown" value={selectedOption} onChange={handleOptionChange}>
+									<option value="dollars">Dollars</option>
+									<option value="shares">Shares</option>
+								</select>
+								<div className="buy-in-amount-box">
+									{selectedOption === "dollars" ? (
+										<div className="dollars-box">
+											<div>
+												<div className="amount-text">Amount:</div>
+												<input
+													type="number"
+													placeholder="$0.00"
+													className="input-dollars"
+													value={dollarAmount}
+													onChange={handleDollarChange}
+												></input>
+											</div>
+											<div className="est-box">
+												<div className="est-quantity">Est. Quantity:</div>
+												<div className="est-quantity-num">
+													{dollarAmount > 0 ? (dollarAmount / company.price).toFixed(6) : 0}
+												</div>
+											</div>
+										</div>
+									) : (
+										<div className="shares-box">
+											<div>
+												<div className="amount-text">Shares:</div>
+												<input
+													type="number"
+													placeholder="0"
+													className="input-shares"
+													value={sharesAmount}
+													onChange={handleShareChange}
+												></input>
+											</div>
+											<div className="market-price-box">
+												<div className="market-price-box-text">Market Price:</div>
+												<div>${company.price}</div>
+											</div>
+											<div className="est-box">
+												<div className="est-quantity">Est. Cost:</div>
+												<div className="est-quantity-num">
+													${sharesAmount > 0 ? (company.price * sharesAmount).toFixed(2) : 0}
+												</div>
+											</div>
+										</div>
+									)}
+									<div className="review-order-box">
+										<div className="rev-button">
+											{!showButtons && <button onClick={handleReviewOrder}>Review Order</button>}
+										</div>
+										{showButtons && (
+											<div className="confirm-cancel-buttons">
+												<button onClick={handleConfirmOrderBuy}>Confirm Order</button>
+												<button onClick={handleCancelOrder}>Cancel Order</button>
+											</div>
+										)}
+									</div>
+									<div className="buy-power-box">
+										<div className="buy-power">{`$${sessionUser.balance.toFixed(2)} buying power available`}</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					) : (
+						<div className="buy-in-box">
+							<div>
+								Sell In:
+								<select id="dropdown" value={selectedOption} onChange={handleOptionChange}>
+									<option value="dollars">Dollars</option>
+									<option value="shares">Shares</option>
+								</select>
+								<div className="buy-in-amount-box">
+									{selectedOption === "dollars" ? (
+										<div className="dollars-box">
+											<div>
+												<div className="amount-text">Amount:</div>
+												<input
+													type="number"
+													placeholder="$0.00"
+													className="input-dollars"
+													value={dollarAmount}
+													onChange={handleDollarChange}
+												></input>
+											</div>
+											<div className="est-box">
+												<div className="est-quantity">Est. Quantity:</div>
+												<div className="est-quantity-num">
+													{dollarAmount > 0 ? (dollarAmount / company.price).toFixed(6) : 0}
+												</div>
+											</div>
+										</div>
+									) : (
+										<div className="shares-box">
+											<div>
+												<div className="amount-text">Shares:</div>
+												<input
+													type="number"
+													placeholder="0"
+													className="input-shares"
+													value={sharesAmount}
+													onChange={handleShareChange}
+												></input>
+											</div>
+											<div className="market-price-box">
+												<div className="market-price-box-text">Market Price:</div>
+												<div>${company.price}</div>
+											</div>
+											<div className="est-box">
+												<div className="est-quantity">Est. Credit:</div>
+												<div className="est-quantity-num">
+													${sharesAmount > 0 ? (company.price * sharesAmount).toFixed(2) : 0}
+												</div>
+											</div>
+										</div>
+									)}
+									<div className="review-order-box">
+										<div className="rev-button">
+											{!showButtons && <button onClick={handleReviewOrder}>Review Order</button>}
+										</div>
+										{showButtons && (
+											<div className="confirm-cancel-buttons">
+												<button onClick={handleConfirmOrderSell}>Confirm Order</button>
+												<button onClick={handleCancelOrder}>Cancel Order</button>
+											</div>
+										)}
+									</div>
+									<div className="buy-power-box">
+										<div className="buy-power">
+											{portfolio[0].price > 0
+												? selectedOption === "dollars"
+													? `$${portfolio[0]?.price.toFixed(2)} Available`
+													: `${portfolio[0]?.shares.toFixed(6)} Shares Available`
+												: selectedOption === "dollars"
+												? "0 Available"
+												: "0 Shares Available"}
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					)}
+					<div className="add-to-list-button">
+						<OpenModalButton
+							buttonText={"+ Add to Lists"}
+							modalComponent={<AddToWatchlistModal company_id={company_id} />}
+						/>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
