fc533aacfdfc89e4f95125baa569a8eafc120d8b
CoryCampbell <corycampbell20@yahoo.com>
2023-11-27 21:20:59 -0600

    progress towards show stocks owned

--- react-app/src/store/companies.js
+++ react-app/src/store/companies.js
@@ -9,55 +9,56 @@ const getCompany = (payload) => {
   };
 };
 
-// const getAllCompanies = (payload) => {
-//   return {
-//     type: GET_ALL_COMPANIES,
-//     payload,
-//   };
-// };
+const getAllCompanies = (payload) => {
+	return {
+		type: GET_ALL_COMPANIES,
+		payload
+	};
+};
 
 //Thunk
 
 export const fetchCompany = (company_id) => async (dispatch) => {
-  //   console.log("company id -------->", company_id);
-  const res = await fetch(`/api/companies/${company_id}`);
+	//   console.log("company id -------->", company_id);
+	const res = await fetch(`/api/companies/${company_id}`);
 
-  const data = await res.json();
-  dispatch(getCompany(data));
-  return data;
+	const data = await res.json();
+	dispatch(getCompany(data));
+	return data;
 };
 
-// export const fetchAllCompanies = () => async (dispatch) => {
-//   const res = await fetch(`/api/companies`);
+export const fetchAllCompanies = () => async (dispatch) => {
+	const res = await fetch(`/api/companies/`);
 
-//   const data = await res.json();
-//   dispatch(getAllCompanies(data));
-//   return data;
-// };
+	const data = await res.json();
+	dispatch(getAllCompanies(data));
+	return data;
+};
 
 const initialState = {
-  company: {},
+	company: {},
+	allCompanies: {}
 };
 
 // Reducer
 export const companyReducer = (state = initialState, action) => {
-  //   console.log(action.type);
-  switch (action.type) {
-    case GET_COMPANY:
-      return {
-        ...state,
-        company: action.payload,
-      };
+	//   console.log(action.type);
+	switch (action.type) {
+		case GET_COMPANY:
+			return {
+				...state,
+				company: action.payload
+			};
 
-    // case GET_ALL_COMPANIES:
-    //   return {
-    //     ...state,
-    //     // allCompanies: action.payload,
-    //   };
+		case GET_ALL_COMPANIES:
+			return {
+				...state,
+				allCompanies: action.payload
+			};
 
-    default:
-      return state;
-  }
+		default:
+			return state;
+	}
 };
 
 export default companyReducer;
