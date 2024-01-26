9b92d16673d63c923981c7af45815a68064ae1ce
CoryCampbell <corycampbell20@yahoo.com>
2024-01-26 15:23:13 -0600

    start of profile dropdown changes

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -1,78 +1,59 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/session";
-import OpenModalButton from "../OpenModalButton";
-import LoginFormModal from "../LoginFormModal";
-import SignupFormModal from "../SignupFormModal";
 import { NavLink } from "react-router-dom";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import AddFunds from "../AddFunds.js";
 
 function ProfileButton({ user, isLoaded }) {
-  const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
-  const ulRef = useRef();
-  const history = useHistory();
+	const dispatch = useDispatch();
+	const [showMenu, setShowMenu] = useState(false);
+	const ulRef = useRef();
+	const history = useHistory();
 
-  const openMenu = () => {
-    if (showMenu) return;
-    setShowMenu(true);
-  };
+	const openMenu = () => {
+		if (showMenu) return;
+		setShowMenu(true);
+	};
 
-  useEffect(() => {
-    if (!showMenu) return;
+	useEffect(() => {
+		if (!showMenu) return;
 
-    const closeMenu = (e) => {
-      if (!ulRef.current.contains(e.target)) {
-        setShowMenu(false);
-      }
-    };
+		const closeMenu = (e) => {
+			if (!ulRef.current.contains(e.target)) {
+				setShowMenu(false);
+			}
+		};
 
-    document.addEventListener("click", closeMenu);
+		document.addEventListener("click", closeMenu);
 
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
+		return () => document.removeEventListener("click", closeMenu);
+	}, [showMenu]);
 
-  const handleLogout = (e) => {
-    e.preventDefault();
-    dispatch(logout());
-    closeMenu();
-    history.push("/");
-  };
+	const handleLogout = (e) => {
+		e.preventDefault();
+		dispatch(logout());
+		closeMenu();
+		history.push("/");
+	};
 
-  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
-  const closeMenu = () => setShowMenu(false);
+	const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+	const closeMenu = () => setShowMenu(false);
 
-  return (
+	return (
 		<>
 			<button className="account-button" onClick={openMenu}>
 				Account
 			</button>
 			<ul className={ulClassName} ref={ulRef}>
-				{isLoaded && user ? (
-					<>
+				{isLoaded && user && (
+					<div className="user-options">
 						<NavLink to={`/transactions/${user.id}`}>Profile</NavLink>
-						<li>
-							<button onClick={handleLogout}>Log Out</button>
-						</li>
+						<button onClick={handleLogout}>Log Out</button>
 						<div style={{ width: "100px" }}>
 							<AddFunds />
 						</div>
-					</>
-				) : (
-					<>
-						{/* <OpenModalButton
-              buttonText="Log In"
-              onItemClick={closeMenu}
-              // modalComponent={<LoginFormModal />}
-            />
-
-            <OpenModalButton
-              buttonText="Sign Up"
-              onItemClick={closeMenu}
-              modalComponent={<SignupFormModal />}
-            /> */}
-					</>
+					</div>
 				)}
 			</ul>
 		</>
