f2f1e101e48905391f73191ec607599d2594ecd7
CoryCampbell <corycampbell20@yahoo.com>
2024-01-26 14:46:22 -0600

    add watchlist css changes to match other modal styles - Cory

--- react-app/src/components/Portfolio/index.js
+++ react-app/src/components/Portfolio/index.js
@@ -4,9 +4,8 @@ import "./Portfolio.css";
 import LineChart from "../Chart/LineChart";
 import { useEffect } from "react";
 import { fetchUserPortfolio } from "../../store/portfolio";
-import { fetchAllCompanies, fetchCompany } from "../../store/companies";
+import { fetchAllCompanies } from "../../store/companies";
 import Watchlist from "../Watchlists";
-import { fetchAllWatchlists } from "../../store/watchlists";
 
 const Portfolio = () => {
 	const dispatch = useDispatch();
@@ -26,7 +25,7 @@ const Portfolio = () => {
 			dispatch(fetchUserPortfolio(sessionUser?.id));
 			dispatch(fetchAllCompanies());
 		}
-	}, [dispatch, sessionUser]);
+	}, [dispatch, sessionUser, totalPortfolioValue]);
 
 	return (
 		<div className="main-portfolio-container">
@@ -46,37 +45,46 @@ const Portfolio = () => {
 						<div className="mod discover-mod">
 							DISCOVER
 							<div>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-								dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-								ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-								fugiat nulla
+								Explore a world of investment opportunities in our Discover section. Uncover potential stocks, gain
+								insights into market trends, and stay informed about the latest financial news. Whether you're a
+								seasoned investor or just starting, our curated selection of stocks and comprehensive market analyses
+								empower you to make informed decisions. Dive into a wealth of information, discover hidden gems, and
+								take control of your financial journey with our intuitive tools and expert recommendations.
 							</div>
 						</div>
 						<div className="mod trending-mod">
 							TRENDING
 							<div>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-								dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-								ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-								fugiat nulla
+								Keep your finger on the pulse of the market with our Trending section. Stay ahead of the curve as you
+								explore the stocks and investment trends that are capturing the attention of traders and investors
+								alike. Discover the hottest opportunities, track emerging market movements, and leverage real-time data
+								to make timely decisions. Our Trending section is your gateway to the dynamic world of finance, helping
+								you identify and capitalize on the latest trends shaping the investment landscape. Whether you're
+								seeking short-term gains or long-term investments, this curated space is designed to guide you through
+								the ever-changing currents of the financial market.
 							</div>
 						</div>
 						<div className="mod learning-mod">
 							LEARNING
 							<div>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-								dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-								ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-								fugiat nulla
+								Empower yourself with knowledge through our Learning section. Unlock a wealth of educational resources
+								designed to enhance your understanding of financial markets and investment strategies. Dive into
+								interactive courses, insightful articles, and engaging tutorials curated to cater to all levels of
+								expertise. Whether you're a novice looking to grasp the basics or a seasoned investor seeking advanced
+								insights, our Learning section provides a comprehensive platform for continuous growth. Equip yourself
+								with the tools to navigate the complexities of finance, make informed decisions, and embark on a journey
+								towards financial literacy and success.
 							</div>
 						</div>
 						<div className="mod news-mod">
 							NEWS
 							<div>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-								dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-								ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-								fugiat nulla
+								Stay informed and up-to-date with our News section. Explore the latest developments in the financial
+								world, from market trends and economic indicators to company announcements and global events. Our
+								curated news feed delivers real-time updates, insightful analyses, and breaking stories that impact the
+								stock market. Whether you're a day trader, long-term investor, or simply curious about the financial
+								landscape, our News section is your go-to source for relevant, reliable, and timely information. Stay
+								ahead of the curve and make well-informed decisions as you navigate the dynamic world of finance.
 							</div>
 						</div>
 					</div>
--- react-app/src/components/WatchlistDetails/DeleteModal.js
+++ react-app/src/components/WatchlistDetails/DeleteModal.js
@@ -1,43 +1,38 @@
-import { useState } from "react"
-import { useModal } from "../../context/Modal"
+import { useModal } from "../../context/Modal";
 import { deleteCompanyThunk } from "../../store/watchlists";
-import { useSelector, useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom/";
+import { useDispatch } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
-function DeleteCompanyFromWatchListModal({companyId, watchlistId}) {
-    const dispatch = useDispatch();
-    const history = useHistory();
-		const [watchlistName, setWatchlistName] = useState("");
-		const [companySymbols, setCompanySymbols] = useState([]);
-		const { closeModal } = useModal();
+function DeleteCompanyFromWatchListModal({ companyId, watchlistId }) {
+	const dispatch = useDispatch();
+	const { closeModal } = useModal();
 
-		const handleConfirm = async (e) => {
-			e.preventDefault();
-			const deleteCompany = await dispatch(deleteCompanyThunk(companyId, watchlistId));
-			closeModal();
-		};
+	const handleConfirm = async (e) => {
+		e.preventDefault();
+		const deleteCompany = await dispatch(deleteCompanyThunk(companyId, watchlistId));
+		closeModal();
+	};
 
-		return (
-			<div className="company-delete-modal-container">
-				<div className="company-delete-upper-container">
-					<FontAwesomeIcon onClick={() => closeModal()} icon={faXmark} />
-					<h1 className="company-delete-title">Delete Company</h1>
-				</div>
-				<div className="company-confirmation">Are you sure you want to delete this company?</div>
-				<div className="company-cancel-ok">
-					<form onSubmit={handleConfirm}>
-						<button type="submit" className="company-confirm-delete-button">
-							Yes
-						</button>
-						<button className="company-cancel-delete-button" onClick={closeModal}>
-							No
-						</button>
-					</form>
-				</div>
+	return (
+		<div className="company-delete-modal-container">
+			<div className="company-delete-upper-container">
+				<FontAwesomeIcon onClick={() => closeModal()} icon={faXmark} />
+				<h1 className="company-delete-title">Delete Company</h1>
 			</div>
-		);
+			<div className="company-confirmation">Are you sure you want to delete this company?</div>
+			<div className="company-cancel-ok">
+				<form onSubmit={handleConfirm}>
+					<button type="submit" className="company-confirm-delete-button">
+						Yes
+					</button>
+					<button className="company-cancel-delete-button" onClick={closeModal}>
+						No
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 }
 
-export default DeleteCompanyFromWatchListModal
+export default DeleteCompanyFromWatchListModal;
